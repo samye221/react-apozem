b757cd028b19e7c1b82377a0e2b56e3db7212688
samye221 <samlambert221@gmail.com>
2020-08-30 11:33:23 +0200

    Updates

---
+++ precache-manifest.eb958b5e4991b35d38ef29b975153aa8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9653a0e78e6f321996841d316b341ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13bab8d941cfa7def269",
+    "url": "/static/css/main.40160248.chunk.css"
+  },
+  {
+    "revision": "735c3f06714fe7256c1e",
+    "url": "/static/js/2.7c16dbe7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7c16dbe7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13bab8d941cfa7def269",
+    "url": "/static/js/main.1c04c573.chunk.js"
+  },
+  {
+    "revision": "858cf38ccd891929cbad",
+    "url": "/static/js/runtime-main.3d9fad1c.js"
+  },
+  {
+    "revision": "f9f02ed05aa86534c3842d44cb20d6c4",
+    "url": "/static/media/Futura Book font.f9f02ed0.ttf"
+  },
+  {
+    "revision": "4cbe95e7f457c6432f5016b355f667c5",
+    "url": "/static/media/logo.4cbe95e7.png"
+  }
+]);
